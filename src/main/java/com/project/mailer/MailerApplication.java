97936be221f8aeef2ab354087c9cf386e288f4ac
suyashsr <suyashsr@nova.tech>
2020-07-16 14:47:36 +0530

    Initial commit

---
+++ src/main/java/com/project/mailer/MailerApplication.java
@@ -0,0 +1,13 @@
+package com.project.mailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailerApplication.class, args);
+	}
+
+}
