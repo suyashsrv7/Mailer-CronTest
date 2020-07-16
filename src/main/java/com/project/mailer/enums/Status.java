765c577f52a818f99252d23ba5323a4559d6b19d
suyashsr <suyashsr@nova.tech>
2020-07-16 19:00:59 +0530

    Add dependency and model

---
+++ src/main/java/com/project/mailer/enums/Status.java
@@ -0,0 +1,8 @@
+package com.project.mailer.enums;
+
+public enum Status {
+    IN_QUEUE,
+    STALLED,
+    FAILED,
+    COMPLETED
+}
---
+++ src/main/java/com/project/mailer/model/Mails.java
@@ -0,0 +1,46 @@
+package com.project.mailer.model;
+
+
+import com.project.mailer.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name="MAILS")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Mails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "tracking_id")
+    private long trackingId;
+
+    @Column(name = "recepient_email")
+    private String recepientEmail;
+
+    @Column(name = "recepient_name")
+    private String recepientName;
+
+    @Lob
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "modified_at")
+    private Timestamp modifiedAt;
+
+    @Column(name = "mail_status")
+    @Enumerated(EnumType.STRING)
+    private Status mailStatus;
+}
